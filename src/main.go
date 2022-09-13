2b3e1fb34fad133867d9d7148907b10d9491c488
bubo-py <kchru@softserveinc.com>
2022-09-13 11:54:42 +0200

    add CLI and serve command

--- src/main.go
+++ src/main.go
@@ -3,16 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	serve()
+	app := &cli.App{}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "serve",
+			Usage: "start the HTTP service",
+			Action: func(*cli.Context) error {
+				serve()
+				return nil
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func serve() {
 	r := chi.NewRouter()
+
+	log.Println("Started an HTTP server on port 8080")
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hi"))
